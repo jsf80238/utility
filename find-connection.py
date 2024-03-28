3f963379b3ad2cbdbfd9e58ab79cbfcb6207a372
Jason Friedman <jsf80238@gmail.com>
2024-03-28 10:12:40 -0600

    .

--- find-connection.py
+++ find-connection.py
@@ -1,27 +1,16 @@
 import argparse
 import csv
-import enum
-# import glob
 import os
 import re
 
-
-class Field(enum.StrEnum):
-    company = "Company"
-    first_name = "First Name"
-    last_name = "Last Name"
-    position = "Position"
-
+COMPANY = "Company"
 
 parser = argparse.ArgumentParser()
 parser.add_argument('pattern')
 parser.add_argument('input', nargs='+', type=argparse.FileType('r', encoding='UTF-8'))
 args = parser.parse_args()
-pattern = re.compile(".*" + args.pattern + ".*", re.IGNORECASE)
+pattern = re.compile(".*" + args.pattern + ".*")
 file_io_list = args.input
-
-# pattern = re.compile(".*mitsu.*", re.IGNORECASE)
-# file_io_list = "/home/jason/Employment/*conn*csv"
 
 found_count = 0
 
@@ -29,11 +18,10 @@ for file_io in file_io_list:
     is_filename_printed = False
     reader = csv.DictReader(file_io)
     for row in reader:
-        if not row[Field.company]:
-            continue
-        if match := pattern.search(row[Field.company]):
+        match = pattern.search(row[COMPANY], re.IGNORECASE)
+        if match:
             if not is_filename_printed:
                 print()
                 print(os.path.basename(file_io.name))
                 is_filename_printed = True
-            print(f"  {row[Field.first_name]} {row[Field.last_name]} ({row[Field.position]} at {row[Field.company]})")
+            print("  " + ",".join(row.values()))
