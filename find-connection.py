de1418f361d710bf305948a87a627ade85c10614
Jason Friedman <jsf80238@gmail.com>
2024-04-01 10:05:36 -0600

    .

--- find-connection.py
+++ find-connection.py
@@ -9,7 +9,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('pattern')
 parser.add_argument('input', nargs='+', type=argparse.FileType('r', encoding='UTF-8'))
 args = parser.parse_args()
-pattern = re.compile(".*" + args.pattern + ".*")
+pattern = re.compile(".*" + args.pattern + ".*", re.IGNORECASE)
 file_io_list = args.input
 
 found_count = 0
@@ -18,7 +18,7 @@ for file_io in file_io_list:
     is_filename_printed = False
     reader = csv.DictReader(file_io)
     for row in reader:
-        match = pattern.search(row[COMPANY], re.IGNORECASE)
+        match = pattern.search(row[COMPANY])
         if match:
             if not is_filename_printed:
                 print()
