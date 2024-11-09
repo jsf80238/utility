f1e45bd3b1c7a542639fc7de6ce94586bcbc0fd8
Jason Friedman <jsf80238@gmail.com>
2024-11-09 10:31:27 -0700

    Add argument specifying letters which _must_ be used.

--- solve-wordle.py
+++ solve-wordle.py
@@ -4,11 +4,13 @@ import re
 DEFAULT_DICTIONARY_FILE = "/usr/share/dict/american-english"  # On Ubuntu at least
 
 parser = argparse.ArgumentParser(description='Solve wordscapes problems.', epilog='Example patterns: "b?ll"  "____"  "ca_"  "mo?se"')
-parser.add_argument('letters', metavar="LETTERS", help="Letters which could be used in the solution, case-insensitive.")
+parser.add_argument('allowed', metavar="ALLOWED_LETTERS", help="Letters which can be used in the solution, case-insensitive.")
+parser.add_argument('required', metavar="REQUIRED_LETTERS", help="Letters which must be used in the solution, case-insensitive. These are automatically added to the 'allowed' list if not already present.")
 parser.add_argument('pattern', metavar="PATTERN", help="Enter constraints here. Use underscores or question marks for the letters to be filled in/guessed.")
 parser.add_argument('--dictionary-file', metavar='/path/to/dictionary', default=DEFAULT_DICTIONARY_FILE, help=f'Depends on operating system, default is {DEFAULT_DICTIONARY_FILE}.')
 args = parser.parse_args()
-allowed_letters = args.letters
+allowed_letters = args.allowed
+required_letters = args.required
 pattern = args.pattern
 dictionary_file = args.dictionary_file
 solution_length = len(pattern)
@@ -21,6 +23,8 @@ match = regex.match(pattern)
 if not match:
     parser.error("Letters, underscores and question marks only for second argument.")
 
+# Letters in the must list should be merged into the allowed list
+allowed_letters = set(allowed_letters) | set(required_letters)
 # Get valid words
 valid_word_set = set([line.strip().lower() for line in open(dictionary_file).readlines()])
 
@@ -29,6 +33,10 @@ for potential_match in valid_word_set:
     if len(potential_match) != 5:
         continue
     is_clear = True
+    for letter in required_letters:
+        if letter not in potential_match:
+            is_clear = False
+            break
     for letter in potential_match:
         if letter not in allowed_letters:
             is_clear = False
