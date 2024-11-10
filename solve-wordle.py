f2734d85fd6de656148a3ed52890dcbe4861fe8e
Jason Friedman <jsf80238@gmail.com>
2024-11-10 08:19:32 -0700

    Add argument specifying which positions disqualify a candidate.

--- solve-wordle.py
+++ solve-wordle.py
@@ -2,16 +2,19 @@ import argparse
 import itertools
 import re
 DEFAULT_DICTIONARY_FILE = "/usr/share/dict/american-english"  # On Ubuntu at least
+WORD_LENGTH = 5
 
 parser = argparse.ArgumentParser(description='Solve wordscapes problems.', epilog='Example patterns: "b?ll"  "____"  "ca_"  "mo?se"')
 parser.add_argument('allowed', metavar="ALLOWED_LETTERS", help="Letters which can be used in the solution, case-insensitive.")
-parser.add_argument('required', metavar="REQUIRED_LETTERS", help="Letters which must be used in the solution, case-insensitive. These are automatically added to the 'allowed' list if not already present.")
 parser.add_argument('pattern', metavar="PATTERN", help="Enter constraints here. Use underscores or question marks for the letters to be filled in/guessed.")
+parser.add_argument('--required', metavar="REQUIRED_LETTERS", help="Letters which must be used in the solution, case-insensitive. These are automatically added to the 'allowed' list if not already present.")
+parser.add_argument('--anti-pattern', metavar="ANTI_PATTERN", help="Where letters do NOT appear. Use underscores or question marks for the positions about which you have no information.")
 parser.add_argument('--dictionary-file', metavar='/path/to/dictionary', default=DEFAULT_DICTIONARY_FILE, help=f'Depends on operating system, default is {DEFAULT_DICTIONARY_FILE}.')
 args = parser.parse_args()
 allowed_letters = args.allowed
-required_letters = args.required
+required_letters = args.required or list()
 pattern = args.pattern
+anti_pattern = args.anti_pattern or "_" * WORD_LENGTH
 dictionary_file = args.dictionary_file
 solution_length = len(pattern)
 regex = re.compile("^[a-zA-Z]+$")
@@ -29,25 +32,32 @@ allowed_letters = set(allowed_letters) | set(required_letters)
 valid_word_set = set([line.strip().lower() for line in open(dictionary_file).readlines()])
 
 match_set = set()
-for potential_match in valid_word_set:
-    if len(potential_match) != 5:
+for candidate in valid_word_set:
+    if len(candidate) != WORD_LENGTH:
         continue
+    # candidate = "deere"
     is_clear = True
-    for letter in required_letters:
-        if letter not in potential_match:
+    # Reject if the candidate contains a letter not in allowed letters
+    for letter in candidate:
+        if letter not in allowed_letters:
             is_clear = False
             break
-    for letter in potential_match:
-        if letter not in allowed_letters:
+    # Reject if the candidate does not contain a required letter
+    for letter in required_letters:
+        if letter not in candidate:
+            is_clear = False
+            break
+    # Verify it does not match the anti-pattern
+    for i in range(WORD_LENGTH):
+        if candidate[i] == anti_pattern[i]:
             is_clear = False
             break
     if not is_clear:
         continue
-    # Now check if it matches the pattern given by the user
+    # Now check if it matches the pattern
     search_pattern_text = "(" + pattern.replace("_", r"\w").replace("?", r"\w") + ")"
     regex = re.compile(search_pattern_text, re.IGNORECASE)
-    match = regex.match(potential_match)
-    if match:
+    if match := regex.match(candidate):
         match_set.add(match.group(1).lower())
 
 print("\n".join(sorted(match_set)))
